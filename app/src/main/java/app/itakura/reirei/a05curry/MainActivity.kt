8bb5edd5616e8d5a9acea280ed8bd3be6691c43c
mirei <usrmii301@gmail.com>
2021-02-06 21:12:23 +0900

    文字の色を変えられた

--- app/src/main/java/app/itakura/reirei/a05curry/MainActivity.kt
+++ app/src/main/java/app/itakura/reirei/a05curry/MainActivity.kt
@@ -1,5 +1,6 @@
 package app.itakura.reirei.a05curry
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     val text = arrayOf("大吉","中吉","小吉","凶")
     val item = arrayOf("カレー","パソコン","水筒","お気に入りのペン","ぬいぐるみ","メガネ","香水")
     val color = arrayOf("赤","ピンク","オレンジ","黄色","緑","青","白","黒")
+
 
 
 
@@ -33,9 +35,27 @@ class MainActivity : AppCompatActivity() {
         val item = item[itemIndex]
         itemTextView.text = item
 
+        val randomColor = Random()
+        val colorIndex = randomColor.nextInt(8)
+        val color = color[colorIndex]
+        colorTextView.text = color
+
+        when(colorIndex){
+            0->colorTextView.setTextColor(Color.RED)
+            1->colorTextView.setTextColor(Color.rgb(227,20,172))
+            2->colorTextView.setTextColor(Color.rgb(227,134,20))
+            3->colorTextView.setTextColor(Color.YELLOW)
+            4->colorTextView.setTextColor(Color.GREEN)
+            5->colorTextView.setTextColor(Color.BLUE)
+            6->colorTextView.setTextColor(Color.WHITE)
+            7->colorTextView.setTextColor(Color.BLACK)
+        }
+
+
+
         //foodTextView.text = food[Random().nextInt(4)]+"を"
 
-        colorTextView.text = color[Random().nextInt(8)]
+        //colorTextView.text = color[Random().nextInt(8)]
 
 
 //        if (Random().nextInt(100)<50){
@@ -45,4 +65,4 @@ class MainActivity : AppCompatActivity() {
 //            verbTextView.textSize = 36.0f
 //        }
     }
-}1
+}
